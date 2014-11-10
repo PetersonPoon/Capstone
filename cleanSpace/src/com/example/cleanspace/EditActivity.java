899863e52fd531a61e305e2a408083b33e801105
joaleo <jch.leong@gmail.com>
2014-11-10 10:59:11 -0800

    App Code - Edit/Details Pages

--- cleanSpace/src/com/example/cleanspace/EditActivity.java
+++ cleanSpace/src/com/example/cleanspace/EditActivity.java
@@ -1,21 +1,25 @@
 package com.example.cleanspace;
 
-import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class EditActivity extends Activity {
 
+	public static final String EDITEDTITLE = "temp";
+	public static final String EDITEDSAMPLEAREA = "123";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit);
 		
-		// Set up action bar
-//		ActionBar actionBar = getActionBar();
-//	    actionBar.setDisplayHomeAsUpEnabled(true);
+
 	}
 
 	@Override
@@ -36,4 +40,24 @@ public class EditActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+
+	
+	public void saveEditActivity(View view) {
+		EditText newSensorTitle = (EditText) findViewById(R.id.sensor_title);
+		String newTitle = newSensorTitle.getText().toString();
+		
+		EditText sampleArea = (EditText) findViewById(R.id.sample_area);
+		String newSampleArea = sampleArea.getText().toString();
+		
+		Intent detailsIntent = new Intent(EditActivity.this,
+				DetailsActivity.class);
+		// For future when we need to pass data to DetailsActivity:
+		// detailsIntent.putExtra(name, value);
+		detailsIntent.putExtra(EDITEDTITLE, newTitle);
+		detailsIntent.putExtra(EDITEDSAMPLEAREA, newSampleArea);
+		
+		EditActivity.this.startActivity(detailsIntent);
+	}
+	
 }
